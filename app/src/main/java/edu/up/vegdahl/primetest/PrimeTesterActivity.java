9b9109d31ccb122f506e2d9089902c51495c6a26
vegdahl <vegdahl@up.edu>
2015-07-28 16:21:42 -0700

    Modified intentation.

--- app/src/main/java/edu/up/vegdahl/primetest/PrimeTesterActivity.java
+++ app/src/main/java/edu/up/vegdahl/primetest/PrimeTesterActivity.java
@@ -23,53 +23,53 @@ super.onCreate(savedInstanceState);
 setContentView(R.layout.activity_prime_tester);
 
 // initialize our resource instance variables
- primeString = this.getString(R.string.is_prime);
- notPrimeString = this.getString(R.string.not_prime);
- answerField = (TextView) findViewById(R.id.resultTextView);
- inputField = (EditText) findViewById(R.id.inputField);
+primeString = this.getString(R.string.is_prime);
+notPrimeString = this.getString(R.string.not_prime);
+answerField = (TextView) findViewById(R.id.resultTextView);
+inputField = (EditText) findViewById(R.id.inputField);
 
- // define event callback: when 'enter' key (e.g., "done"), we call the isPrime method
- // in the PrimeTesterActivity class, and set the result field to be the appropriate message.
- inputField.setOnKeyListener(new View.OnKeyListener() {
- public boolean onKey(View v, int keyCode, KeyEvent event) {
- if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
- String input = String.valueOf(inputField.getText());
- String result;
- try {
- long value = Long.valueOf(input);
- result = PrimeTester.isPrime(value) ? primeString : notPrimeString;
- } catch (NumberFormatException nfx) {
- result = notPrimeString;
- }
- answerField.setText(result);
- return true;
- }
- return false;
- }
- });
- }
+// define event callback: when 'enter' key (e.g., "done"), we call the isPrime method
+// in the PrimeTesterActivity class, and set the result field to be the appropriate message.
+inputField.setOnKeyListener(new View.OnKeyListener() {
+public boolean onKey(View v, int keyCode, KeyEvent event) {
+if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+String input = String.valueOf(inputField.getText());
+String result;
+try {
+long value = Long.valueOf(input);
+result = PrimeTester.isPrime(value) ? primeString : notPrimeString;
+} catch (NumberFormatException nfx) {
+result = notPrimeString;
+}
+answerField.setText(result);
+return true;
+}
+return false;
+}
+});
+}
 
- @Override
- public boolean onCreateOptionsMenu(Menu menu) {
- // Inflate the menu; this adds items to the action bar if it is present.
- getMenuInflater().inflate(R.menu.menu_prime_tester, menu);
- return true;
- }
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+getMenuInflater().inflate(R.menu.menu_prime_tester, menu);
+return true;
+}
 
- @Override
- public boolean onOptionsItemSelected(MenuItem item) {
- // Handle action bar item clicks here. The action bar will
- // automatically handle clicks on the Home/Up button, so long
- // as you specify a parent activity in AndroidManifest.xml.
- int id = item.getItemId();
+@Override
+public boolean onOptionsItemSelected(MenuItem item) {
+// Handle action bar item clicks here. The action bar will
+// automatically handle clicks on the Home/Up button, so long
+// as you specify a parent activity in AndroidManifest.xml.
+int id = item.getItemId();
 
- //noinspection SimplifiableIfStatement
- if (id == R.id.action_settings) {
- return true;
- }
+//noinspection SimplifiableIfStatement
+if (id == R.id.action_settings) {
+return true;
+}
 
- return super.onOptionsItemSelected(item);
- }
+return super.onOptionsItemSelected(item);
+}
 }
 
 
